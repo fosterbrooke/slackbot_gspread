931e33bc7f2c56ff1fba27d4d5d746db77ab22e6
fosterbrooke <okisahandsome@gmail.com>
2024-08-10 17:44:38 -0500

    first commit

---
+++ msg2slack.py
@@ -0,0 +1,33 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# Setup the credentials for Google Sheets
+# scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+scope = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+creds = ServiceAccountCredentials.from_json_keyfile_name('./credential.json', scope)
+client = gspread.authorize(creds)
+
+# Open the Google Sheet by ID and get the data
+spreadsheet_id = '1XkD01mutrZtemL4I5wQWqtPKhl-k5ugCkszf9XYObSY'
+sheet = client.open_by_key(spreadsheet_id).sheet1
+slack_ids = sheet.col_values(1)  # Assumes Slack IDs are in the first column
+
+# Setup the Slack client
+slack_token = 'xoxb-6252476882098-7543158897207-baYMvEKBmLJzYALdJEKcYRig'
+client = WebClient(token=slack_token)
+
+# Define your custom message
+message = "Hello, this is a custom message!"
+
+# Send messages to each Slack ID
+for slack_id in slack_ids:
+    try:
+        response = client.chat_postMessage(
+            channel=slack_id,
+            text=message
+        )
+        print(f"Message sent to {slack_id}: {response['ok']}")
+    except SlackApiError as e:
+        print(f"Error sending message to {slack_id}: {e.response['error']}")
